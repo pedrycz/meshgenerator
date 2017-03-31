3762dfbd8d479ac7296db1a0b279b5179f9a18d5
ppedrycz <piotrekpedrycz@gmail.com>
2017-03-31 18:16:27 +0200

    Removed unnecessary package

--- src/main/java/pl/edu/agh/iet/gg/meshgenerator/productions/P2a.java
+++
@@ -1,5 +0,0 @@
-package pl.edu.agh.iet.gg.meshgenerator.productions;
-
-public class P2a extends Production {
-
-}
--- src/main/java/pl/edu/agh/iet/gg/meshgenerator/productions/Production.java
+++
@@ -1,5 +0,0 @@
-package pl.edu.agh.iet.gg.meshgenerator.productions;
-
-public abstract class Production {
-
-}
