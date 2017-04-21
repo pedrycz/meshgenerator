4b01e6ba2e64a22d854b9da62d6e65a7865eaac3
bgrochal <bartek.grochal@gmail.com>
2017-04-21 18:08:35 +0200

    Mechanism for reading config file implemented.

---
+++ src/main/java/pl/edu/agh/iet/gg/meshgenerator/visualization/config/Config.java
@@ -0,0 +1,63 @@
+package pl.edu.agh.iet.gg.meshgenerator.visualization.config;
+
+import pl.edu.agh.iet.gg.meshgenerator.visualization.GraphVisualiser;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author Bartłomiej Grochal
+ */
+public class Config {
+
+    private static Properties properties;
+
+
+    private Config() {
+    }
+
+
+    public static String getString(String key) {
+        Properties properties = getProperties();
+        return properties.getProperty(key);
+    }
+
+    public static int getInteger(String key) {
+        Properties properties = getProperties();
+        try {
+            return Integer.parseInt(properties.getProperty(key));
+        } catch (NumberFormatException exc) {
+            return 0;
+        }
+    }
+
+    public static double getDouble(String key) {
+        Properties properties = getProperties();
+        try {
+            return Double.parseDouble(properties.getProperty(key));
+        } catch (NumberFormatException exc) {
+            return 0.0;
+        }
+    }
+
+
+    private static Properties getProperties() {
+        if (properties == null) {
+            openPropertiesFile();
+        }
+        return properties;
+    }
+
+    private static void openPropertiesFile() {
+        properties = new Properties();
+        try (InputStream inputStream =
+                     new FileInputStream(GraphVisualiser.class.getResource("config/visualizer.properties").getPath())) {
+            properties.load(inputStream);
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+    }
+
+}
