2cd3030ade8e59d361deb2afa061949b275f5fb9
Mat <majarosz.j50@gmail.com>
2017-05-17 10:21:09 +0200

    add P2c and P2d tests

--- src/test/java/pl/edu/agh/iet/gg/meshgenerator/P2Test.java
+++ src/test/java/pl/edu/agh/iet/gg/meshgenerator/P2Test.java
@@ -23,6 +23,10 @@ public class P2Test {
 
         //then
         assert(i.getNW().getBelow().get().getNE().getSE().equals(i.getNE().getBelow().get().getNW().getSW()));
+        assert(i.getNW().getBelow().get().getSE().getNE().equals(i.getNE().getBelow().get().getSW().getNW()));
+        assert(i.getNW().getBelow().get().getNE().getE().get().equals(i.getNE().getBelow().get().getNW()));
+        assert(i.getNW().getBelow().get().getSE().getE().get().equals(i.getNE().getBelow().get().getSW()));
+
     }
 
     @Test
@@ -39,6 +43,48 @@ public class P2Test {
 
         //then
         assert(i.getNW().getBelow().get().getSE().getSW().equals(i.getSW().getBelow().get().getNE().getNW()));
+        assert(i.getNW().getBelow().get().getSW().getSE().equals(i.getSW().getBelow().get().getNW().getNE()));
+        assert(i.getNW().getBelow().get().getSE().getS().get().equals(i.getSW().getBelow().get().getNE()));
+        assert(i.getNW().getBelow().get().getSW().getS().get().equals(i.getSW().getBelow().get().getNW()));
+    }
+
+    @Test
+    public void productionP2cShouldJoinNodesHorizontally() throws Exception {
+        //given
+        Graph graph = new Graph();
+        graph.getRoot().applyP1();
+        I i = graph.getRoot().getBelow().get();
+
+        //when
+        i.getSW().applyP1();
+        i.getSE().applyP1();
+        i.applyP2c(i.getSW(), i.getSE());
+
+        //then
+        assert(i.getSW().getBelow().get().getSE().getNE().equals(i.getSE().getBelow().get().getSW().getNW()));
+        assert(i.getSW().getBelow().get().getNE().getSE().equals(i.getSE().getBelow().get().getNW().getSW()));
+        assert(i.getSW().getBelow().get().getNE().getE().get().equals(i.getSE().getBelow().get().getNW()));
+        assert(i.getSW().getBelow().get().getSE().getE().get().equals(i.getSE().getBelow().get().getSW()));
+    }
+
+    @Test
+    public void productionP2dShouldJoinNodesVertically() throws Exception {
+        //given
+        Graph graph = new Graph();
+        graph.getRoot().applyP1();
+        I i = graph.getRoot().getBelow().get();
+
+        //when
+        i.getNE().applyP1();
+        i.getSE().applyP1();
+        i.applyP2d(i.getNE(), i.getSE());
+
+        //then
+        assert(i.getNE().getBelow().get().getSW().getSE().equals(i.getSE().getBelow().get().getNW().getNE()));
+        assert(i.getNE().getBelow().get().getSE().getSW().equals(i.getSE().getBelow().get().getNE().getNW()));
+        assert(i.getNE().getBelow().get().getSE().getS().get().equals(i.getSE().getBelow().get().getNE()));
+        assert(i.getNE().getBelow().get().getSW().getS().get().equals(i.getSE().getBelow().get().getNW()));
+
     }
 
 }
